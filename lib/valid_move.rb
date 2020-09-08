eef0d39705213c72373faf368d4bf2b44bed4273
Benjamin Stein <whereyouben@gmail.com>
2020-09-08 03:24:39 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 
 def valid_move?(board, index)
   if index.between?(0,8) == true
-    if position_taken?(board, index) == true
+    if position_taken?(board, index) == false
       true
     end
   else
